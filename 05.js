494d6045625e3192ba478dd860e97141d624e940
DANYSENA79 <DANYSENA101@GMAIL.COM>
2024-07-29 11:13:45 -0300

    Update 05.js

--- 05.js
+++ 05.js
@@ -14,6 +14,8 @@ function pluck(array, propiedad) {
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
+  var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
+  retum pluck.map('name');
 
 }
 
